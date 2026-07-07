d62c5ecd61058e3c817ec0ab86089b3a00bd362c
Arshad <shaikarshad87643@gmail.com>
2026-07-07 22:23:57 +0530

    created the student entity

---
+++ StudentManagementSystem/src/main/java/com/Arshad/StudentManagementSystem/Entity/student.java
@@ -0,0 +1,74 @@
+package com.Arshad.StudentManagementSystem.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name = "Students")
+public class student {
+    private String name;
+    private long id;
+    private int age ;
+    private String email;
+    private String department;
+
+    public student(String name, long id, int age, String email, String department) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.email = email;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
