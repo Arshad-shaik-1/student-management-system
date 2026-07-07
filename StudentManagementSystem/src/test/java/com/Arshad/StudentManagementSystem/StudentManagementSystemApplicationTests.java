acbec0b9ec7c8744ccf5195bf22953bf362ef85e
Arshad <shaikarshad87643@gmail.com>
2026-07-07 22:10:25 +0530

    Initial Spring Boot project setup

---
+++ StudentManagementSystem/src/test/java/com/Arshad/StudentManagementSystem/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Arshad.StudentManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
